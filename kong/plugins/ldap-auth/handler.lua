caae3995e0ec3bf3ab0e2837994dfd8710a0973f
Shashi Ranjan <shashi@mashape.com>
2016-03-22 17:23:49 -0700

    wip

--- kong/plugins/ldap-auth/handler.lua
+++ kong/plugins/ldap-auth/handler.lua
@@ -7,32 +7,6 @@ function LdapAuthHandler:new()
   LdapAuthHandler.super.new(self, "ldap-auth")
 end
 
-function LdapAuthHandler:certificate(conf)
-  LdapAuthHandler.super.certificate(self)
-  local ssl = require "ngx.ssl"
-  ssl.clear_certs()
-
-  local data = cache.get_or_set(cache.ldap_ssl_data(ngx.ctx.api.id), function()
-    local result = {
-      cert_der = ngx.decode_base64(conf._cert_der_cache),
-      key_der = ngx.decode_base64(conf._key_der_cache),
-      cacert_der = ngx.decode_base64(conf._cert_der_cache)
-    }
-    return result
-  end)
-
-  local ok, err = ssl.set_der_cert(data.cert_der)
-  if not ok then
-    ngx.log(ngx.ERR, "failed to set DER cert: ", err)
-    return
-  end
-  ok, err = ssl.set_der_priv_key(data.key_der)
-  if not ok then
-    ngx.log(ngx.ERR, "failed to set DER private key: ", err)
-    return
-  end
-end
-
 function LdapAuthHandler:access(conf)
   LdapAuthHandler.super.access(self)
   access.execute(conf)
@@ -41,4 +15,3 @@ end
 LdapAuthHandler.PRIORITY = 1000
 
 return LdapAuthHandler
-
--- kong/plugins/ldap-auth/schema.lua
+++ kong/plugins/ldap-auth/schema.lua
@@ -1,59 +1,17 @@
 local DaoError = require "kong.dao.error"
 local constants = require "kong.constants"
 
-local function validate_cert(v)
-  local ssl = require "ngx.ssl"
-  local der = ssl.cert_pem_to_der(v)
-  if der then
-    return true, nil, { _cert_der_cache = base64.encode(der) }
-  end
-  return false, "Invalid SSL certificate"
-end
-
-local function validate_cacert(v)
-  local ssl = require "ngx.ssl"
-  local der = ssl.cert_pem_to_der(v)
-  if der then
-    return true, nil, { _cacert_der_cache = base64.encode(der) }
-  end
-  return false, "Invalid SSL certificate"
-end
-
-local function validate_key(v)
-  local ssl = require "ngx.ssl"
-  local der = ssl.priv_key_pem_to_der(v)
-  if der then
-    return true, nil, { _key_der_cache = base64.encode(der) }
-  end
-  return false, "Invalid SSL certificate key"
-end
-
 return {
 fields = {
     ldap_host = {required = true, type = "string"},
     ldap_port = {required = true, type = "number"},
     start_tls = {required = true, type = "boolean", default = false},
-    authenticate_server = {required = true, type = "boolean", default = false},
-    cert = {required = false, type = "string", func = validate_cert},
-    key = {required = false, type = "string", func = validate_key},
-    cert_chain = {required = false, type = "string", func = validate_cacert},
+    verify_ldap_host = {required = true, type = "boolean", default = false},
     base_dn = {required = true, type = "string"},
     attribute = {required = true, type = "string"},
     cache_ttl = {required = true, type = "number", default = 60},
     hide_credentials = {type = "boolean", default = false},
     timeout = {type = "number", default = 10000},
     keepalive = {type = "number", default = 60000},
-    
-    -- Internal use
-    _cert_der_cache = { type = "string" },
-    _key_der_cache = { type = "string" },
-    _cacert_der_cache = { type = "string" }
-  },
-  self_check = function(schema, plugin_t, dao, is_update)
-    if plugin_t["ldap_protocol"] == "ldaps" and plugin_t["start_tls"] then
-      return false, DaoError("You cannot set start_tls to 'true' when protocol is selected as 'ldaps'", constants.DATABASE_ERROR_TYPES.SCHEMA)
-    end
-    
-    return true
-  end
+  }
 }
